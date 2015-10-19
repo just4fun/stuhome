e1d94feea88849279eccdbde00e0bc2d611433e9
just4fun <houritsunohikari@gmail.com>
2015-10-19 17:39:18 +0800

    update menu layout

--- ios/app/components/Home.js
+++ ios/app/components/Home.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFFFFF'
+    backgroundColor: '#fff'
   }
 });
 
--- ios/app/components/Menu.js
+++ ios/app/components/Menu.js
@@ -1,8 +1,10 @@
 import React, {
   Component,
   StyleSheet,
+  TouchableHighlight,
   View,
-  Text
+  Text,
+  Image
 } from 'react-native';
 import Dimensions from 'Dimensions';
 import SideMenu from 'react-native-side-menu';
@@ -12,14 +14,31 @@ const window = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: window.width,
+    width: window.width * 2 / 3,
     height: window.height,
-    backgroundColor: 'gray',
-    padding: 20
+    borderRightWidth: 1,
+    borderRightColor: '#ddd',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 30,
+  },
+  menuHeader: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    paddingLeft: 30,
+    paddingBottom: 15
+  },
+  avatar: {
+    height: 70,
+    width: 70
+  },
+  row: {
+    flexDirection: 'row',
   },
   item: {
     fontSize: 18,
-    paddingTop: 10
+    paddingTop: 10,
+    paddingLeft: 30,
+    paddingBottom: 10,
   }
 });
 
@@ -29,11 +48,31 @@ export default class Menu extends Component {
     this.props.navigateTo(route);
   }
 
+  _pressAvatar() {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.item} onPress={() => this._navigateTo({id: 'home', title: '最新'})}>最新</Text>
-        <Text style={styles.item} onPress={() => this._navigateTo({id: 'forumList', title: '版块'})}>版块</Text>
+        <View style={styles.menuHeader}>
+          <Image style={styles.avatar}
+            source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
+            onPress={this._pressAvatar.bind(this)}
+          />
+        </View>
+        <TouchableHighlight
+          style={styles.row}
+          underlayColor='#ddd'
+          onPress={() => this._navigateTo({id: 'home', title: '最新'})}>
+          <Text style={styles.item}>最新</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.row}
+          underlayColor='#ddd'
+          onPress={() => this._navigateTo({id: 'forumList', title: '版块'})}>
+          <Text style={styles.item}>版块</Text>
+        </TouchableHighlight>
       </View>
     );
   }
