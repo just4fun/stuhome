fe0fe8e9c00530679359a57e465a0d8b7e7db11a
just4fun <houritsunohikari@gmail.com>
2015-11-01 15:52:21 +0800

    get rid of babel and webpack with upgrading react-native to 0.13.X

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import React, { AppRegistry } from 'react-native';
-import Root from './containers/Root';
+import Root from './ios/app/containers/Root';
 
 AppRegistry.registerComponent('stuhome', () => Root);
