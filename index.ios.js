b76713353afc7f1be66f9e6a4754eb6b81d446b0
just4fun <houritsunohikari@gmail.com>
2016-02-03 21:39:44 +0800

    change the code structure

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import React, { AppRegistry } from 'react-native';
-import Root from './ios/app/containers/Root';
+import Root from './app/containers/Root';
 
 AppRegistry.registerComponent('stuhome', () => Root);
