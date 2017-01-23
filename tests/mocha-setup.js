5cc60957fd7d291fd7d26caff03513636d7cd3cb
just4fun <houritsunohikari@gmail.com>
2017-01-23 13:24:01 +0800

    test: introduce unit testing infrastructure with enzyme, mocha and chai

---
+++ tests/mocha-setup.js
@@ -0,0 +1,8 @@
+// certain features such as ES6 modules are not yet supported in Node,
+// when we add a module which uses ES6 import statements, we should use
+// babel to compile them.
+//
+// http://stackoverflow.com/a/35045012/2798001
+require('babel-register')({
+  ignore: /node_modules\/(?!react-native-image-progress|react-native-progress)/
+});
