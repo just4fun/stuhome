fc57f0d8040917b6cf7fcddabdd0f6183f5f5ff9
just4fun <houritsunohikari@gmail.com>
2017-02-04 19:52:53 +0800

    refactor: use Symbol as action name, but it's dependent on https://github.com/redux-saga/redux-saga/pull/781

--- src/actions/topic/topicListAction.js
+++ src/actions/topic/topicListAction.js
@@ -1,15 +1,15 @@
 import { createAction } from 'redux-actions';
 
-export const REQUEST = 'REQUEST';
-export const INVALIDATE = 'INVALIDATE';
-export const RESET = 'RESET';
+export const REQUEST = Symbol();
+export const INVALIDATE = Symbol();
+export const RESET = Symbol();
 export const fetchTopicList = createAction(REQUEST);
 export const invalidateTopicList = createAction(INVALIDATE);
 export const resetTopicList = createAction(RESET);
 
-export const REQUEST_STARTED = 'REQUEST_STARTED';
-export const REQUEST_COMPELTED = 'REQUEST_COMPELTED';
-export const REQUEST_FAILED = 'REQUEST_COMPELTED';
+export const REQUEST_STARTED = Symbol();
+export const REQUEST_COMPELTED = Symbol();
+export const REQUEST_FAILED = Symbol();
 export const request = createAction(REQUEST_STARTED);
 // return 2nd argument as `meta` field
 export const success = createAction(REQUEST_COMPELTED, null, (...args) => args[1]);
