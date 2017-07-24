8cb906f2b57bb5ba87515e92a94c3d6e7cc190a7
just4fun <houritsunohikari@gmail.com>
2017-07-24 16:08:23 +0800

    test: add test for #15

--- tests/mocha-setup.js
+++ tests/mocha-setup.js
@@ -4,5 +4,5 @@
 //
 // http://stackoverflow.com/a/35045012/2798001
 require('babel-register')({
-  ignore: /node_modules\/(?!react-native-image-progress|react-native-progress)/
+  ignore: /node_modules\/(?!react-native-image-progress|react-native-progress|react-native-vector-icons)/
 });
