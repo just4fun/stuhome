21c29da715a81d3b3b5e72236a388da20ad8ec7f
just4fun <houritsunohikari@gmail.com>
2017-12-11 16:44:43 +0800

    fix: expand the clickable area for menu icon at left top

--- src/components/button/MenuButton.js
+++ src/components/button/MenuButton.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+  View,
+  TouchableHighlight
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AlertCount from '../AlertCount';
 import styles from '../../styles/components/button/_MenuButton';
+import colors from '../../styles/common/_colors';
 
 export default class MenuButton extends Component {
   render() {
     let alertCount = this.props.alertCount;
 
     return (
-      <View style={styles.container}>
-        <Icon
-          style={styles.menu}
-          name='reorder'
-          size={18}
-          onPress={() => this.props.updateMenuState(true)} />
-        {!!alertCount && <AlertCount style={styles.alert} doNotShowCount={true} />}
-      </View>
+      <TouchableHighlight
+        style={styles.container}
+        underlayColor={colors.underlay}
+        onPress={() => this.props.updateMenuState(true)}>
+        <View style={styles.view}>
+          <Icon
+            style={styles.menu}
+            name='reorder'
+            size={18} />
+          {!!alertCount && <AlertCount style={styles.alert} doNotShowCount={true} />}
+        </View>
+      </TouchableHighlight>
     );
   }
 }
