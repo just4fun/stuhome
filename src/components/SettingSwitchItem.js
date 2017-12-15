95e591949338d4440a51663626f099c4415b5c77
just4fun <houritsunohikari@gmail.com>
2017-12-15 21:42:17 +0800

    feat (WIP): save the value of notification switch to AsyncStorage

--- src/components/SettingSwitchItem.js
+++ src/components/SettingSwitchItem.js
@@ -9,13 +9,16 @@ import styles from '../styles/components/_SettingSwitchItem';
 
 export default class SettingItem extends Component {
   render() {
-    let { text, style } = this.props;
+    let { text, style, value, onValueChange } = this.props;
 
     return (
       <View style={[mainStyles.item, style]}>
         <Text style={mainStyles.info}>{text}</Text>
         <View style={styles.indicator}>
-          <Switch style={styles.switch} />
+          <Switch
+            style={styles.switch}
+            onValueChange={onValueChange}
+            value={value} />
         </View>
       </View>
     );
--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -5,6 +5,7 @@ import {
   Text,
   Image,
   AlertIOS,
+  AsyncStorage
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { ImageCache } from "react-native-img-cache";
@@ -16,6 +17,23 @@ import mainStyles from '../styles/components/_Main';
 import styles from '../styles/containers/_About';
 
 export default class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enableNotification: false
+    };
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('settings.notification')
+                // The value stored in AsyncStorage is string.
+                .then(value => {
+                  debugger;
+                  this.setState({ enableNotification: value === 'true' })
+                });
+  }
+
   clearCache() {
     AlertIOS.alert(
       '提示',
@@ -29,16 +47,22 @@ export default class Settings extends Component {
     );
   }
 
+  handleNotificationValueChange(value) {
+    // The value to store should be string, hence `toString()` here.
+    AsyncStorage.setItem('settings.notification', value.toString())
+                .then(() => {
+                  debugger;
+                  this.setState({ enableNotification: value })
+                });
+  }
+
   render() {
-    let {
-      router,
-      alertCount
-    } = this.props;
+    let { router } = this.props;
 
     return (
       <View style={[mainStyles.container, styles.container]}>
         <Header title='设置'>
-          <PopButton router={this.props.router} />
+          <PopButton router={router} />
         </Header>
         <View style={styles.group}>
           <SettingItem
@@ -47,7 +71,9 @@ export default class Settings extends Component {
         </View>
         <View style={styles.group}>
           <SettingSwitchItem
-            text='消息提醒' />
+            text='消息提醒'
+            onValueChange={(value) => this.handleNotificationValueChange(value)}
+            value={this.state.enableNotification} />
         </View>
       </View>
     );
