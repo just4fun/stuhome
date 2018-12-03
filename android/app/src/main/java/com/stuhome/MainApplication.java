009c045799069b819c5cef8d043c99e92b3920ec
just4fun <houritsunohikari@gmail.com>
2018-12-03 13:17:43 +0800

    chore: upgrade react-native to 0.57.7, react to 16.6.1

--- android/app/src/main/java/com/stuhome/MainApplication.java
+++ android/app/src/main/java/com/stuhome/MainApplication.java
@@ -3,6 +3,8 @@ package com.stuhome;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -31,6 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new PickerPackage(),
             new PickerPackage(),
             new VectorIconsPackage()
       );
